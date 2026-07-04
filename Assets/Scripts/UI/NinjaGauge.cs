7b63e364f7edd80c3fa74a425395e63124322661
William Lebel <william.leb@hotmail.com>
2026-07-04 05:09:01 -0400

    8(

--- Assets/Scripts/UI/NinjaGauge.cs
+++ Assets/Scripts/UI/NinjaGauge.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NinjaGauge : MonoBehaviour {
+	private const int MAX_NUMBER_OF_ITEMS = 4;
+	
 	[SerializeField]
 	private Image mainNinjaSignImage;
+	
+	[SerializeField]
+	private HorizontalLayoutGroup ninaGaugeItemLayout;
+	
+	[SerializeField]
+	private GameObject ninjaGaugeItem;
+	
+	private List<GameObject> ninjaGaugeItems = new List<GameObject>();
+	private NinjaSignDescriptor previousNinjaSign = null;
 
 	private void Start() {
 		if (NinjaSignVessel.HasIsntance(gameObject.scene)) {
@@ -15,6 +26,8 @@ public class NinjaGauge : MonoBehaviour {
 		if (mainNinjaSignImage) {
 			mainNinjaSignImage.sprite = null;
 		}
+
+		ClearItems();
 	}
 
 	private void OnDestroy() {
@@ -28,11 +41,57 @@ public class NinjaGauge : MonoBehaviour {
 		if (mainNinjaSignImage) {
 			mainNinjaSignImage.sprite = addedNinjaSign.Icon;
 		}
+
+		if (previousNinjaSign != null) {
+			AddItemFor(previousNinjaSign);
+		}
+		previousNinjaSign = addedNinjaSign;
 	}
 
 	private void OnNinjaCombinationExecuted(NinjaSignCombination executedNinjaCombination) {
 		if (mainNinjaSignImage) {
 			mainNinjaSignImage.sprite = null;
 		}
+		
+		ClearItems();
+	}
+
+	private void AddItemFor(NinjaSignDescriptor ninjaSign) {
+		if (ninjaSign == null) {
+			return;
+		}
+
+		if (!ninjaGaugeItem) {
+			return;
+		}
+
+		if (!ninaGaugeItemLayout) {
+			return;
+		}
+
+		if (ninjaGaugeItems.Count == MAX_NUMBER_OF_ITEMS) {
+			DestroyImmediate(ninjaGaugeItems[0]);
+			ninjaGaugeItems.RemoveAt(0);
+		}
+
+		GameObject newNinjaGaugeItem = Instantiate(ninjaGaugeItem, ninaGaugeItemLayout.transform);
+		Image imageComponent = newNinjaGaugeItem.GetComponent<Image>();
+		if (imageComponent) {
+			imageComponent.sprite = ninjaSign.Icon;
+		}
+		
+		ninjaGaugeItems.Add(newNinjaGaugeItem);
+	}
+
+	private void ClearItems() {
+		if (ninaGaugeItemLayout) {
+			foreach (Transform child in ninaGaugeItemLayout.transform) 
+			{
+				Destroy(child.gameObject);
+			}
+		}
+		
+		ninjaGaugeItems.Clear();
+		previousNinjaSign = null;
 	}
 }
