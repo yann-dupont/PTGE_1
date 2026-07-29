89f7b0199f5950e9d30160bf01ff062f7037f779
Yann Dupont <yanndupont0@gmail.com>
2026-07-29 20:24:18 +0900

    Rollers

--- Assets/Scripts/PlayerControls/InputSystem_Actions.cs
+++ Assets/Scripts/PlayerControls/InputSystem_Actions.cs
@@ -449,7 +449,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""1c04ea5f-b012-41d1-a6f7-02e963b52893"",
-                    ""path"": ""<Mouse>/rightButton"",
+                    ""path"": ""<Keyboard>/e"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""Keyboard&Mouse"",
